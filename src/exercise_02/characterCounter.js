2ee763609f2ab4a5c9fac10609a91a26670beaa0
liyixuan <13069579956@163.com>
2020-09-04 17:24:07 +0800

    liyiuxuan feat :exercise_02's test

--- src/exercise_02/characterCounter.js
+++ src/exercise_02/characterCounter.js
@@ -6,5 +6,15 @@ export default function countCharacter(string, prediction) {
   // * Please implement this function and pass all the tests in character_counter_spec.js.
   // * Please do NOT modify the signature of the function.
 
-  throw new Error('Please delete this line and implement the function');
+  if (string == null || undefined) { return 0; }
+  if (string === '') { return 0; }
+  if (prediction !== null) {
+    const count = 0;
+    if (string.indexOf(prediction) !== -1)  { 
+      count++;
+      countCharacter(string.substring(string.indexOf(prediction) + prediction.length()), prediction);
+      return count; }
+    
+    return string.length;
+  } 
 }
