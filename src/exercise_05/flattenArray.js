8d77b354c9159353c39a370a8f5fae6d09860d68
liyixuan <13069579956@163.com>
2020-09-05 09:30:33 +0800

    liyiuxan feat: exercise_05's test

--- src/exercise_05/flattenArray.js
+++ src/exercise_05/flattenArray.js
@@ -6,5 +6,17 @@ export default function flattenArray(array) {
   // * Please implement this function and pass all the tests in flatten_array_spec.js.
   // * Please do NOT modify the signature of the function.
 
-  throw new Error('Please delete this line and implement the function');
+  if (array == null || undefined) { throw new Error('Flatten undefined or null array'); }
+  const stack = [...array];
+  const res = [];
+  while (stack.length) {
+    const next = stack.pop();
+    if (Array.isArray(next)) {
+      stack.push(...next);
+    }
+    else {
+      res.push(next);
+    }
+  }
+  return res.reverse();
 }
