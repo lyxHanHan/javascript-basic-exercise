f847f72de7b5a74bd30294436170bdbd12fd2dd2
liyixuan <13069579956@163.com>
2020-09-05 09:53:09 +0800

    liyixuan feat: exercise_04

--- src/exercise_04/arrayToList.js
+++ src/exercise_04/arrayToList.js
@@ -5,6 +5,7 @@ export default function arrayToList(array) {
   //
   // * Please implement this function and pass all the tests in array_to_list_spec.js.
   // * Please do NOT modify the signature of the function.
-
-  throw new Error('Please delete this line and implement the function');
+  if (array == null || undefined) { throw new Error('Creating list from undefined array'); }
+  if (array.length === 0) { throw new Error('Creating list from empty array'); }  
+  array.value = array[0];
 }
